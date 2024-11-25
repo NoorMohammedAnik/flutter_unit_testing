55613d380208e398a9ea5cd97d45052dd28fc7c9
Noor Mohammed Anik <noormohammedanik@gmail.com>
2024-11-25 19:21:07 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/myapp/flutter_unit_testing/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.myapp.flutter_unit_testing
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ test/calculate_salary_test.dart
@@ -0,0 +1,44 @@
+
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_unit_testing/my_function.dart';
+
+void main()
+{
+
+
+  //for a single unit test
+  test("Simple Test", (){
+
+    MyFunction mf=MyFunction();
+
+    int salary=mf.calculateSalary(1);
+    expect(salary, 500);
+
+  });
+
+
+  //for multiple unit tests
+  group("salary test", (){
+
+    test("Salary Test 1", (){
+
+      MyFunction mf=MyFunction();
+
+      int salary=mf.calculateSalary(10);
+      expect(salary, 5000);
+
+    });
+
+    test("Salary Test 2", (){
+
+      MyFunction mf=MyFunction();
+
+      int salary=mf.calculateSalary(5);
+      expect(salary, 2500);
+
+
+    });
+
+
+  });
+}
